5731e8cf7072f2543e9bd8a6276b276caa0417c2
AbeerAB <60429676+AbeerAB@users.noreply.github.com>
2020-03-12 10:23:33 +0300

    Update User.java

--- User.java
+++ User.java
@@ -5,6 +5,15 @@ public class User {
     String name ,username,password ;
     int age ;
 
+    
+     public User(String username,String password) {
+        this.username=username;
+        this.password=password;
+        this.name="";
+        this.age=-1;
+
+    }
+    
     public User(String name,int age ,String username,String password) {
        this.name=name;
        this.age=age;
@@ -12,12 +21,6 @@ public class User {
        this.password=password;
     }
 
-    public User(String username,String password) {
-        this.username=username;
-        this.password=password;
-        this.name="";
-        this.age=-1;
-
-    }
+   
 
 }
